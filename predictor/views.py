3a49efc40e6fc0b5ae4ba7d20f0be76e2d662068
Emmydantoch <emmanueltdaniel@yahoo.com>
2025-07-17 13:12:29 +0100

    firstcommit

---
+++ predictor/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+# Train model (use database in real project)
+X = np.array([[1.5], [2], [3], [4.5], [5], [6], [7.5], [8], [9]])
+y = np.array([20, 25, 35, 55, 60, 65, 75, 85, 90])
+model = LinearRegression()
+model.fit(X, y)
+
+def home(request):
+    result = None
+    if request.method == 'POST':
+        try:
+            hours = float(request.POST.get("hours"))
+            predicted = model.predict([[hours]])
+            result = round(predicted[0], 2)
+        except:
+            result = "Invalid input!"
+    return render(request, 'predictor/home.html', {'result': result})
+
+# Create your views here.
